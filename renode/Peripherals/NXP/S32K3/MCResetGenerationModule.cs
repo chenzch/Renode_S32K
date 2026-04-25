e865f9cc3c88f52a91536e5a82e999bf98509de0
Peter Chen <chenwilsoft@msn.com>
2026-04-25 22:01:22 +0800

    Add MC_RGM.FES

--- renode/Peripherals/NXP/S32K3/MCResetGenerationModule.cs
+++ renode/Peripherals/NXP/S32K3/MCResetGenerationModule.cs
@@ -10,14 +10,6 @@ namespace Antmicro.Renode.Peripherals.NXP.S32K3
     // 自定义外设：32 位寄存器映射的简单设备
     public class MCResetGenerationModule : IPeripheral, IDoubleWordPeripheral, IKnownSize
     {
-        // 寄存器偏移定义（相对基地址，非绝对）
-        private enum Registers : long
-        {
-            Control = 0x00,   // 控制寄存器
-            Data = 0x04,      // 数据寄存器
-            Status = 0x08     // 状态寄存器
-        }
-
         // 寄存器字段（使用 Register Framework 简化管理）
         private readonly DoubleWordRegisterCollection _registers;
 
@@ -35,7 +27,7 @@ namespace Antmicro.Renode.Peripherals.NXP.S32K3
         {
             ResetValue value = ResetValue.Load(resetValue);
             // 控制寄存器：bit0 使能，bit1 中断使能
-            _registers.DefineRegister((long)Registers.Control, resetValue: value.Get("MC_RGM.DES", 0x01, this.Log))
+            _registers.DefineRegister(0x00, resetValue: value.Get("MC_RGM.DES", 0x01, this.Log))
                 .WithFlag(0, out _DES_F_POR, name: "F_POR", writeCallback: (oldVal, newVal) => {_DES_F_POR.Value = (newVal ? false : oldVal);})
                 .WithFlag(3, out _DES_FCCU_FTR, name: "FCCU_FTR", writeCallback: (oldVal, newVal) => {_DES_FCCU_FTR.Value = (newVal ? false : oldVal);})
                 .WithFlag(4, out _DES_STCU_URF, name: "STCU_URF", writeCallback: (oldVal, newVal) => {_DES_STCU_URF.Value = (newVal ? false : oldVal);})
@@ -52,19 +44,26 @@ namespace Antmicro.Renode.Peripherals.NXP.S32K3
                 .WithFlag(29, out _DES_SW_DEST, name: "SW_DEST", writeCallback: (oldVal, newVal) => {_DES_SW_DEST.Value = (newVal ? false : oldVal);})
                 .WithFlag(30, out _DES_DEBUG_DEST, name: "DEBUG_DEST", writeCallback: (oldVal, newVal) => {_DES_DEBUG_DEST.Value = (newVal ? false : oldVal);});
 
+            _registers.DefineRegister(0x08, resetValue: value.Get("MC_RGM.FES", 0x00, this.Log))
+                .WithFlag(0, out _FES_F_EXR, name: "F_EXR", writeCallback: (oldVal, newVal) => {_FES_F_EXR.Value = (newVal ? false : oldVal);})
+                .WithFlag(3, out _FES_FCCU_RST, name: "FCCU_RST", writeCallback: (oldVal, newVal) => {_FES_FCCU_RST.Value = (newVal ? false : oldVal);})
+                .WithFlag(4, out _FES_ST_DONE, name: "ST_DONE", writeCallback: (oldVal, newVal) => {_FES_ST_DONE.Value = (newVal ? false : oldVal);})
+                .WithFlag(6, out _FES_SWT0_RST, name: "SWT0_RST", writeCallback: (oldVal, newVal) => {_FES_SWT0_RST.Value = (newVal ? false : oldVal);})
+                .WithFlag(7, out _FES_SWT1_RST, name: "SWT1_RST", writeCallback: (oldVal, newVal) => {_FES_SWT1_RST.Value = (newVal ? false : oldVal);})
+                .WithFlag(8, out _FES_SWT2_RST, name: "SWT2_RST", writeCallback: (oldVal, newVal) => {_FES_SWT2_RST.Value = (newVal ? false : oldVal);})
+                .WithFlag(9, out _FES_JTAG_RST, name: "JTAG_RST", writeCallback: (oldVal, newVal) => {_FES_JTAG_RST.Value = (newVal ? false : oldVal);})
+                .WithFlag(10, out _FES_SWT3_RST, name: "SWT3_RST", writeCallback: (oldVal, newVal) => {_FES_SWT3_RST.Value = (newVal ? false : oldVal);})
+                .WithFlag(12, out _FES_PLL_AUX, name: "PLL_AUX", writeCallback: (oldVal, newVal) => {_FES_PLL_AUX.Value = (newVal ? false : oldVal);})
+                .WithFlag(16, out _FES_HSE_SWT_RST, name: "HSE_SWT_RST", writeCallback: (oldVal, newVal) => {_FES_HSE_SWT_RST.Value = (newVal ? false : oldVal);})
+                .WithFlag(20, out _FES_HSE_BOOT_RST, name: "HSE_BOOT_RST", writeCallback: (oldVal, newVal) => {_FES_HSE_BOOT_RST.Value = (newVal ? false : oldVal);})
+                .WithFlag(29, out _FES_SW_FUNC, name: "SW_FUNC", writeCallback: (oldVal, newVal) => {_FES_SW_FUNC.Value = (newVal ? false : oldVal);})
+                .WithFlag(30, out _FES_DEBUG_FUNC, name: "DEBUG_FUNC", writeCallback: (oldVal, newVal) => {_FES_DEBUG_FUNC.Value = (newVal ? false : oldVal);});
+
+
             // 数据寄存器：可读可写
-            _registers.DefineRegister((long)Registers.Data, resetValue: 0x00)
+            _registers.DefineRegister(0x04, resetValue: 0x00)
                 .WithValueField(0, 32, out _dataField, name: "DATA",
                     writeCallback: (oldVal, newVal) => _dataBuffer = (uint)newVal);
-
-            // 状态寄存器：bit0 忙，bit1 中断标志
-            _registers.DefineRegister((long)Registers.Status, resetValue: 0x00)
-                .WithFlag(0, out _busyBit, name: "BUSY")
-                .WithFlag(1, out _interruptFlagBit, name: "INTERRUPT_FLAG",
-                    writeCallback: (oldVal, newVal) =>
-                    {
-                        if (newVal) _interruptFlagBit.Value = false; // 写 0 清中断
-                    });
         }
 
         // ---------------- 接口实现 ----------------
@@ -85,8 +84,6 @@ namespace Antmicro.Renode.Peripherals.NXP.S32K3
         {
             _registers.Reset();
             _dataBuffer = 0x00;
-            _busyBit.Value = false;
-            _interruptFlagBit.Value = false;
             this.Log(LogLevel.Info, "MCResetGenerationModule 已复位");
         }
 
@@ -110,9 +107,21 @@ namespace Antmicro.Renode.Peripherals.NXP.S32K3
         private IFlagRegisterField _DES_SW_DEST;
         private IFlagRegisterField _DES_DEBUG_DEST;
 
+        private IFlagRegisterField _FES_F_EXR;
+        private IFlagRegisterField _FES_FCCU_RST;
+        private IFlagRegisterField _FES_ST_DONE;
+        private IFlagRegisterField _FES_SWT0_RST;
+        private IFlagRegisterField _FES_SWT1_RST;
+        private IFlagRegisterField _FES_SWT2_RST;
+        private IFlagRegisterField _FES_JTAG_RST;
+        private IFlagRegisterField _FES_SWT3_RST;
+        private IFlagRegisterField _FES_PLL_AUX;
+        private IFlagRegisterField _FES_HSE_SWT_RST;
+        private IFlagRegisterField _FES_HSE_BOOT_RST;
+        private IFlagRegisterField _FES_SW_FUNC;
+        private IFlagRegisterField _FES_DEBUG_FUNC;
+
         private IValueRegisterField _dataField;
-        private IFlagRegisterField _busyBit;
-        private IFlagRegisterField _interruptFlagBit;
         private uint _dataBuffer;
     }
 }
